2c54c65e7344e612b9680022d2e9da645c28d660
kimseonj <kimsunjun1221@gmail.com>
2025-04-06 03:11:17 +0900

    fix: User 정책 추가로 인한 코드 수정

--- src/main/java/com/bubble/buubleforprofessor/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/bubble/buubleforprofessor/user/service/impl/UserServiceImpl.java
@@ -6,11 +6,14 @@ import com.bubble.buubleforprofessor.user.repository.UserRepository;
 import com.bubble.buubleforprofessor.user.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import java.sql.Timestamp;
 
+@Slf4j
 @Service
 @Transactional(rollbackOn = Exception.class)
 @RequiredArgsConstructor
@@ -31,9 +34,18 @@ public class UserServiceImpl implements UserService {
                 .loginId(joinRequestDto.getLoginId())
                 .name(joinRequestDto.getUserName())
                 .password(bCryptPasswordEncoder.encode(joinRequestDto.getPassword()))
+                .email("test@test.com")
+                .phoneNumber("010-1111-2222")
+                .createdAt(new Timestamp(System.currentTimeMillis()))
                 .build();
 
-        userRepository.save(user);
+        try {
+            userRepository.save(user);
+            userRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            log.debug("insert fail - 무결서 위반", e);
+            return "insert fail";
+        }
 
         return "join Success";
     }
