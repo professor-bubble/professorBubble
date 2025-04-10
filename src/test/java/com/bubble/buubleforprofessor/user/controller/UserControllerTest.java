c35f0b41e585cd741c6acc9a7658c7db3bd134b3
hwan4338 <rlackdghks8@naver.com>
2025-04-10 20:52:30 +0900

    test : 유저 최초 채팅방 입장시 채팅방 생성 기능 테스트 추가

--- src/test/java/com/bubble/buubleforprofessor/user/controller/UserControllerTest.java
+++ src/test/java/com/bubble/buubleforprofessor/user/controller/UserControllerTest.java
@@ -1,9 +1,11 @@
 package com.bubble.buubleforprofessor.user.controller;
 
 import com.bubble.buubleforprofessor.chatroom.dto.ChatroomDetailResponseDto;
+import com.bubble.buubleforprofessor.chatroom.dto.ChatroomEnterRequestDto;
 import com.bubble.buubleforprofessor.chatroom.dto.ChatroomResponseDto;
 import com.bubble.buubleforprofessor.chatroom.dto.MessageDto;
 import com.bubble.buubleforprofessor.chatroom.service.ChatroomService;
+import com.bubble.buubleforprofessor.chatroom.service.ChatroomUserService;
 import com.bubble.buubleforprofessor.skin.dto.SkinResponseDto;
 import com.bubble.buubleforprofessor.skin.service.SkinService;
 import com.bubble.buubleforprofessor.user.dto.ApprovalRequestCreateDto;
@@ -11,6 +13,7 @@ import com.bubble.buubleforprofessor.user.dto.ProfessorResponseDto;
 import com.bubble.buubleforprofessor.user.dto.UserSimpleResponseDto;
 import com.bubble.buubleforprofessor.user.service.ProfessorService;
 import com.bubble.buubleforprofessor.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -55,6 +58,9 @@ class UserControllerTest {
 
     @MockitoBean
     private UserService userService;
+
+    @MockitoBean
+    private ChatroomUserService chatroomUserService;
 
     @BeforeEach
     void setUp() {
@@ -237,7 +243,7 @@ class UserControllerTest {
         // 서비스 메서드가 정확히 한 번 호출되었는지 검증
         verify(skinService, times(1)).modifySkinStatus(userId, skinId);
     }
-
+    @DisplayName("유저 아이디에 해당하는 채팅방 리스트 가져오기")
     @WithMockUser(roles = "USER")
     @Test
     void testGetAllChatroomByUserId() throws Exception{
@@ -259,5 +265,22 @@ class UserControllerTest {
         mockMvc.perform(get("/api/users/{userId}/chatrooms",userId)
                 .with(csrf()))
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("채팅방 최초 입장 시 채팅방 생성")
+    @WithMockUser(roles = "USER")
+    @Test
+    void testCreateChatroomUser()throws Exception{
+        UUID userId = UUID.randomUUID();
+        int chatroomId= 1;
+        ChatroomEnterRequestDto chatroomEnterRequestDto=new ChatroomEnterRequestDto("바다");
+        doNothing().when(chatroomUserService).createChatroomUser(userId, chatroomId, chatroomEnterRequestDto);
+        mockMvc.perform(post("/api/users/{userId}/chatroom/{chatroomId}", userId, chatroomId)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(chatroomEnterRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+        verify(chatroomUserService).createChatroomUser(userId, chatroomId, chatroomEnterRequestDto);
     }
 }
