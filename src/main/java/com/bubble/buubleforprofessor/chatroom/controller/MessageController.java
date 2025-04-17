543c8c9c065a52182bdc43f57bbb64d058600d88
hwan4338 <rlackdghks8@naver.com>
2025-04-17 13:57:00 +0900

    feat : 욕설 필터링 기능 추가

--- src/main/java/com/bubble/buubleforprofessor/chatroom/controller/MessageController.java
+++ src/main/java/com/bubble/buubleforprofessor/chatroom/controller/MessageController.java
@@ -9,7 +9,6 @@ import com.bubble.buubleforprofessor.chatroom.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-    private final SimpMessageSendingOperations messagingTemplate;
     private final MessageService messageService;
     private final ChatroomUserService chatRoomUserService;
 
@@ -26,27 +24,17 @@ public class MessageController {
     public void sendMessage(@Payload MessageRequestDto message) {
         chatRoomUserService.exists(message.getUserId(),message.getChatRoomId());
 
+        message = messageService.filtering(message);
         MessageMongo message1 = messageService.save(message);
 
-
         MessageSimpleDto simpleDto = new MessageSimpleDto();
+        simpleDto.setUserId(message.getUserId());
         simpleDto.setUserName(message.getUserName());
         simpleDto.setType(message1.getMessageType());
         simpleDto.setContent(message1.getContent());
         simpleDto.setCreateAt(message1.getSendTime());
-        //유저는 채팅방 구독 , 자기자신 구독.
-        //교수는 자기자신 구독
-        if(message.getRole().equals("PROFESSOR"))
-        {
-            //교수가 보내는곳. 채팅방과(유저 전체) 교수(자신)
-            messagingTemplate.convertAndSend("/sub/chatroom/" + message.getChatRoomId(), simpleDto);
-            messagingTemplate.convertAndSend("/sub/chatroom/" + message.getChatRoomId()+"/professor", simpleDto);
-        }
-        else
-        {
-            //유저가 보내는곳.교수 유저(자신)
-            messagingTemplate.convertAndSend("/sub/chatroom/" + message.getChatRoomId()+"/professor", simpleDto);
-            messagingTemplate.convertAndSend("/sub/user/" + message.getUserId(),simpleDto);
-        }
+
+        String userRole = message.getRole();
+        messageService.send(message.getChatRoomId(),userRole,simpleDto);
     }
 }
--- src/main/java/com/bubble/buubleforprofessor/chatroom/service/MessageService.java
+++ src/main/java/com/bubble/buubleforprofessor/chatroom/service/MessageService.java
@@ -2,10 +2,13 @@ package com.bubble.buubleforprofessor.chatroom.service;
 
 import com.bubble.buubleforprofessor.chatroom.doc.MessageMongo;
 import com.bubble.buubleforprofessor.chatroom.dto.MessageRequestDto;
+import com.bubble.buubleforprofessor.chatroom.dto.MessageSimpleDto;
 import com.bubble.buubleforprofessor.chatroom.entity.Message;
 
 import java.util.UUID;
 
 public interface MessageService {
+    MessageRequestDto filtering(MessageRequestDto messageRequestDto);
     MessageMongo save(MessageRequestDto message);
+    void send(int chatroomId,String userRole,MessageSimpleDto messageSimpleDto);
 }
